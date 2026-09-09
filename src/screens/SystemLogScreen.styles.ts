0e05ebe5f738e34646b95c19a5e960666eb9bc67
Arsène DOBROVOLSKYY <arsened.devweb@gmail.com>
2026-09-09 18:34:54 +0200

    feat(screens): restyle SystemLogScreen

---
+++ src/screens/SystemLogScreen.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const LogBox = styled.div(({ theme }) => ({
+  width: '100%',
+  textAlign: 'left',
+  maxHeight: '16rem',
+  overflowY: 'auto',
+  border: `1px solid ${theme.colors.border}`,
+  borderRadius: theme.radii.sm,
+  background: theme.colors.surfaceAlt,
+  padding: theme.space['3'],
+  fontSize: theme.font.size.sm,
+  lineHeight: theme.font.lineHeight.relaxed,
+}))
+
+export const LogLine = styled.p(({ theme }) => ({
+  whiteSpace: 'pre-wrap',
+  wordBreak: 'break-word',
+  color: theme.colors.textMuted,
+}))
