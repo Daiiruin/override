dbe35e5eed9df43fad2398c61e8a2e8347e9970e
Arsène DOBROVOLSKYY <arsened.devweb@gmail.com>
2026-09-09 18:34:30 +0200

    feat(screens): add divider to FileSystemScreen

--- src/screens/FileSystemScreen.tsx
+++ src/screens/FileSystemScreen.tsx
@@ -1,7 +1,7 @@
 import { useGame } from '../game/GameContext'
 import { getLevel } from '../game/levels'
 import { LevelId } from '../game/types'
-import { ScreenShell } from '../components/ScreenShell'
+import { ScreenShell, Divider } from '../components/ScreenShell'
 import { LevelAnswerForm } from '../components/LevelAnswerForm'
 import { FileTree, type FileNode } from '../components/FileTree'
 
@@ -43,6 +43,7 @@ export function FileSystemScreen() {
     <ScreenShell title={level.title}>
       <p>{level.narrative(state)}</p>
       <FileTree node={ROOT} />
+      <Divider />
       <LevelAnswerForm placeholder="Clé de sauvegarde" />
     </ScreenShell>
   )
